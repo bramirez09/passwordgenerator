4eb1e294ed021241f25e42ab44a4eddec709e027
brecec <brendaramirez@brendas-mbp.lan>
2023-05-02 23:24:35 -0500

    passwordoptions

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,62 @@
 //Array - special characters
-var specialcharacters = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '[', '}', '}', '|', ':', ';', '"', '<', ',', '>', '.', '?', '/', '\\', "'",  ];
+var specialCharacters = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '[', '}', '}', '|', ':', ';', '"', '<', ',', '>', '.', '?', '/', '\\', "'",];
 
 // Array - numerical characters
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
 
 // Array - lowercase
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]; 
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
 
 //Array - uppercase
- var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ]; 
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
 
- // function - get password options
+// function - get password options
 function getPasswordOptions() {
- var userChoice = window.prompt("How many characters would like your password to contain?");
+  var userChoice = window.prompt("How many characters would like your password to contain?");
+  //If user pressed Cancel, immediately end function
+  if (!userChoice) {
+    return;
+  }
+  if (isNaN(userChoice)) {
+    alert("Please input a number.");
+    return;
+  }
 
- // is user pressed Cancel, immediately end function
- if (!userChoice) {
-  return;
- }
+  //^^ validates user is entering number, below - is verifying length is a number
+  var length = Number(userChoice);
 
-  if (length < 8 === true) {
+  // verifies length (is a number) that must be within the parameters
+  if (length < 8) {
     alert('Password length must be a minimum of 8 characters');
-    
+    return;
   }
-// ^issue
-  if (length > 128 === true) {
-    alert ('Password length cannot exceed 129 characters'); 
-    
+
+  if (length > 128) {
+    alert('Password length cannot exceed 129 characters');
+    return;
   }
-  // issue
+
   var includeSpecialCharacters = confirm
     ('Would you like your password to contain special characters?');
 
   var includeLowerCaseCharacters = confirm
     ('Would you like your password to contain lowercase characters?');
 
+
   var includeUpperCaseCharacters = confirm
     ('Would you like your password to contain uppercase characters?');
 
-  var includeNumbers = confirm 
+  var includeNumbers = confirm
     ('Would you like your password to contain numbers?');
 
-  if (userChoice =) {
-    window.prompt (' Must select at least one password criteria.')
+
+  if (includeNumbers ===false && includeUpperCaseCharacters ===false && includeLowerCaseCharacters ===false && includeSpecialCharacters ===false) {
+    alert(' Must select at least one password criteria.');
   }
+}
+
+function getRandom(arr){
+  
 }
 
 // Assignment Code
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# passwordgenerator
+# Password Generator
+
+## Description
+For this project I built a password generator that will create a random password based on the criteria selected by the user. User is able to select character length (between 8-128 characters) and a combination of uppercase, lowercase and special characters. 
+
+## Visuals
+![Alt text](Assets/passwordgeneratordemo.png)
+
+## Installation
+Additional programs or extension installation is not needed, the app will run in the browser. Please select the following Github link: 
+
+## Usage
+User can specify character length (between 8-128 characters long), can confirm whether or not to include lowercase, uppercase, numeric and/or special characters. After criteria is confirmed application will generate password accordinly to user input and selection. 
